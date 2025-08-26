26eb02480ebd0ea1d2e2692c24b09043fb86b92a
yudi topp <yudi.topp@gmail.com>
2025-08-25 23:48:01 -0400

    Add files via upload

---
+++ src/server/core/post.ts
@@ -0,0 +1,16 @@
+import { context, reddit } from '@devvit/web/server';
+
+export const createPost = async () => {
+  const { subredditName } = context;
+  if (!subredditName) {
+    throw new Error('subredditName is required');
+  }
+
+  return await reddit.submitCustomPost({
+    splash: {
+      appDisplayName: 'breaking-crystal',
+    },
+    subredditName: subredditName,
+    title: 'breaking-crystal',
+  });
+};
